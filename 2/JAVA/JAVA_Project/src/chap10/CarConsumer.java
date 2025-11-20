7c9d024945270e2aa1635014a4586d39f1303a3e
jooyoungiili <joo.zero.10@gmail.com>
2025-11-20 23:25:18 +0900

    람다식 해보기

---
+++ 2/JAVA/JAVA_Project/src/chap10/CarConsumer.java
@@ -0,0 +1,5 @@
+package chap10;
+
+public interface CarConsumer {
+    void accept(Car car);
+}
---
+++ 2/JAVA/JAVA_Project/src/chap10/MethodRefDemo.java
@@ -0,0 +1,34 @@
+package chap10;
+
+interface Mathematical { double calculate(double d);}
+interface Pickable { char pick(String s, int i );}
+interface Computable{int compute(int x, int y);}
+
+class Utils{
+    int add(int a, int b) {return a+b;}
+}
+
+public class MethodRefDemo {
+    public static void main(String[] args){
+        Mathematical m;
+        Pickable p;
+        Computable c;
+
+        //m = (double d ) -> {Math.abs(d);};
+        //m = d-> Math.abs(d);
+        //-> 바로 직관적으로 알 수 있는 람다식
+        m = Math::abs;
+        System.out.println(m.calculate(-50.3));
+
+        //p = (a,b) -> a.charAt(b);
+        p = String::charAt;
+        System.out.println(p.pick("안녕, 인스턴스 메서드 참조 !", 4));
+
+        Utils utils = new Utils();
+        c = utils::add;
+        System.out.println(c.compute(20,30));
+
+
+    }
+
+}
